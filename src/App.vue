511b1031e0f36d5f9bdbc210ae38b565334a9496
Mandy Tsai <mandy3152618@gmail.com>
2023-05-19 10:36:16 +0800

    build: 專案安裝 tailwind 與 scss

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <div>
+    <p class="text-6xl text-red-700">Hello!</p>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
