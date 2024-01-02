a84b0daedbecda9363cda8c377582848a4f5f541
Mandy Tsai <mandy3152618@gmail.com>
2024-01-02 12:28:16 +0800

    move: HelloWorld.cy.ts

---
+++ src/components/__test__/HelloWorld.cy.ts
@@ -0,0 +1,12 @@
+import HelloWorld from '../HelloWorld.vue';
+
+describe('props 傳遞', () => {
+  it('msg 應顯示 Hello', () => {
+    cy.mount(HelloWorld, {
+      props: {
+        msg: 'Hello Cypress',
+      },
+    });
+    cy.get('[data-test="title"]').should('have.text', 'Hello Cypress');
+  });
+});
